b9f8464096d22afd2ce71f5a84174b60c49a354a
Radu Lupascu <72099690+radulupascu@users.noreply.github.com>
2023-12-12 00:36:34 +0200

    Update get_artists.py

--- get_artists.py
+++ get_artists.py
@@ -1,40 +1,21 @@
 import openai
 
-def get_gpt_response_content(user_message):
-  # Get GPT responses to messages
-  response = openai.ChatCompletion.create(
-    model = "gpt-4",
-    messages = 
-      user_message
-  )
-  return response["choices"][0]["message"]["content"]
+def get_artists(genre, label):
+    openai.api_key = "KEY"
+    print("label " + str(label))
 
-def parse_gpt_response(response):
-  # Parse GPT response
-  response = response.strip("\n").strip()
-  return response
-
-def get_artists(genre, labels):
-    openai.api_key = "API_KEY"
-
-    print(labels)
-
-    chat_log = []
-    chat_log.append({"role": "system", "content": 
-                       f"Analyze this list of {genre} artists from {labels} and rank them based on popularity: \n"})
-
-    response = get_gpt_response_content(chat_log)
-
-    print(parse_gpt_response(response)) # Replace with output on website
-    chat_log.append({"role": "assistant",
-                    "content": response.strip("\n").strip()})
+    response = openai.Completion.create(
+        engine="text-davinci-003",  # or the latest available engine
+        prompt=f"Analyze this list of {genre} artists from {label} and rank them based on popularity: \n",
+        max_tokens=100
+    )
 
     # Split the response into lines and take the first five lines
     top_five_artists = response.choices[0].text.strip().split('\n')[:5]
 
     # Remove numbers and extra spaces from each artist's name
     artists = [artist.split('. ', 1)[-1].strip() for artist in top_five_artists]
-
+    print(artists)
     return artists
 
 # Example usage
